4b3b9a7c64394d2f316bc9aa681acfdc85e350af
Chuck.Visuals <firstinmath2@gmail.com>
2025-12-13 13:34:07 -0500

    style refactor

---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import HomeScreen from "./screens/home";
+
+export default function HomeRoute() {
+  // Handle any context-specific code in the route, e.g. getting url params
+  return <HomeScreen />;
+}
